082fe27b1bdaaae96f965494422174eb99e8e444
Logan <translucenttomato@gmail.com>
2022-09-29 21:32:28 -0400

    added options

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+
 public class BubbleSort {
 
     public static void printArray(int[] array) {
@@ -24,27 +26,26 @@ public class BubbleSort {
         return array;
     }
 
-    public static void writeArrayToFile(int[] array, String fileName){
-        try  {
-        FileWriter fileWriter = new FileWriter(fileName);
-        for (int a : array) {
-            fileWriter.write(a + "\n");
-            fileWriter.flush();
-        }
-        fileWriter.close();
-        
-    }
-        catch(IOException e){
+    public static void writeArrayToFile(int[] array, String fileName) {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+            for (int a : array) {
+                fileWriter.write(a + "\n");
+                fileWriter.flush();
+            }
+            fileWriter.close();
+
+        } catch (IOException e) {
             System.out.println("Encountered an error while writing to file");
             e.printStackTrace();
         }
     }
 
-    public static int[] readArrayFromFile(String fileName) throws FileNotFoundException{
+    public static int[] readArrayFromFile(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
         ArrayList arrayList = new ArrayList<>();
-        while (scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
             int a = Integer.parseInt(str);
             arrayList.add(a);
@@ -52,7 +53,7 @@ public class BubbleSort {
         scanner.close();
 
         int[] array = new int[arrayList.size()];
-        for (int i = 0; i < arrayList.size(); i++){
+        for (int i = 0; i < arrayList.size(); i++) {
             array[i] = (int) arrayList.get(i);
         }
         return array;
@@ -65,23 +66,48 @@ public class BubbleSort {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
-                } 
+                }
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Bubble Sort");
+        while (true) {
+            System.out.println("1) Sort random array\n2) Type array to sort\n3) Exit");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    int arrayLength;
+                    System.out.println("Enter array length");
+                    arrayLength = scanner.nextInt();
+                    int[] array = createRandomArray(arrayLength);
+                    String fileName = "integers.txt";
+                    printArray(array);
+                    writeArrayToFile(array, fileName);
+                    array = readArrayFromFile(fileName);
+                    bubbleSort(array);
+                    writeArrayToFile(array, "sorted.txt");
+                    break;
+                case 2:
+                    System.out.println("Enter array length");
+                    arrayLength = scanner.nextInt();
+                    array = new int[arrayLength];
+                    System.out.println("Enter array elements");
+                    for (int i = 0; i < arrayLength; i++) {
+                        array[i] = scanner.nextInt();
+                    }
+                    bubbleSort(array);
+                    printArray(array);
+                    break;
+                case 3:
+                    System.out.print("Goodbye");
+                    System.exit(0);
+                    break;
 
-        int arrayLength = 5;
-        int[] array = createRandomArray(arrayLength);
-        String fileName = "integers.txt";
+            }
 
-        printArray(array);
-        writeArrayToFile(array, fileName);
-
-        array = readArrayFromFile(fileName);
-        bubbleSort(array);
-
-        writeArrayToFile(array, "sorted.txt");
+        }
     }
 }
